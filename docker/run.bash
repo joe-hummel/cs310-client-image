845237351fbc8f3b3ca01485b154f5cc4db44ace
Joe Hummel, PhD <joe.hummel@northwestern.edu>
2026-03-11 00:33:38 -0500

    updates to build and run commands

--- docker/run.bash
+++ docker/run.bash
@@ -11,4 +11,4 @@ read -r image < ./docker/_image-name.txt
 #  -v   => map current dir (.) TO home dir
 #  --rm => remove container when done
 #
-docker run -it -u user -w /home/user -v .:/home/user --network="host" --ipc="host" --name "$image" --rm "$image" bash
+docker run -it -u user -w /home/user -v .:/home/user --network host --name "$image" --rm "$image" bash
